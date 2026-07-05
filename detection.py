836326b592d90df9d21e8a9832eba93a2d967287
Jonathan Galli Coli <130164954+jocoga20@users.noreply.github.com>
2026-07-05 19:22:06 +0200

    transfer work

--- detection.py
+++ detection.py
@@ -1,69 +1,66 @@
+import huggingface_hub as hfh
 import torchvision
 import torch
-from torch import nn
 import torchvision.transforms.v2 as t
 from models import rework_model
-from utils import get_train
 from utils import gtsrb_mean, gtsrb_std
+from datasets import load_dataset
+from torchvision.models.detection.rpn import AnchorGenerator
 
+class HuggingfaceDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, transform):
+        self.ds = dataset
+        self.transform = transform
 
-class MyBackbone(nn.Module):
-    def __init__(self, resnet):
-        super().__init__()
+    def __len__(self):
+        return len(self.ds)
+    
+    def __getitem__(self, index):
+        sample = self.ds[index]
+        x = self.transform(sample['image'])
+        y = {'box': sample['objects']['bbox'], 'class': sample['objects']['category']}
+        return x, y
 
-        self.conv1 = resnet.conv1
-        self.bn1 = resnet.bn1
-        self.relu = resnet.relu
-        self.maxpool = resnet.maxpool
-        self.layer1 = resnet.layer1
-        self.layer2 = resnet.layer2
-        self.layer3 = resnet.layer3
-        self.layer4 = resnet.layer4
-
-        self.out_channels = 2048
-
-    def forward(self, x):
-
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        return {"0": x}
+def get_dataset():
+    return load_dataset("keremberke/german-traffic-sign-detection", "full")
+    
 
 def prepare_backbone():
     m = torchvision.models.resnet50(weights=None)
     rework_model(m, last_layer='linear', output_classes=43, do_freeze_backbone=False)
     m.load_state_dict(torch.load('checkpoints/resnet50.acc95.pt')['model'])
-    rework_model(m, last_layer='identity', do_freeze_backbone=True)
-    return MyBackbone(m)
+    m = torch.nn.Sequential(*list(m.children())[:-2])
+    m.out_channels = 2048       # 2048 if resnet50, 512 if resnet18
+    return m
 
 @torch.inference_mode()
 def main():
-    model = torchvision.models.detection.FasterRCNN(backbone=prepare_backbone(), num_classes=43)
+    anchor_generator = AnchorGenerator(
+        sizes=((16,32,64,128,256,512)),
+        aspect_ratios=((1.,),)
+    )
+    model = torchvision.models.detection.FasterRCNN(backbone=prepare_backbone(), num_classes=43, rpn_anchor_generator=anchor_generator)\
+    .cuda()
     model.eval()
-    mytransform = t.Compose([
-        t.Resize((128, 128)),
-        t.ToImage(),
-        t.ToDtype(torch.float32, scale=True),
-        t.Normalize(gtsrb_mean, gtsrb_std)
-    ])
+    model.roi_heads.score_thresh = 0.001
 
-    ds = get_train(transform=mytransform)
     loader = torch.utils.data.DataLoader(
-        dataset=ds,
-        batch_size=2,
+        dataset=HuggingfaceDataset(get_dataset()['test'], transform=t.Compose([
+            t.ToImage(),
+            t.ToDtype(torch.float32, scale=True),
+            t.Normalize(gtsrb_mean, gtsrb_std),
+        ])),
+        batch_size=1,
         shuffle=False,
         pin_memory=True
     )
-    x, y = next(iter(loader))
-    out = model(x)
-    print(out)
 
+    for x, y in loader:
+        out = model(x.cuda())
+        print(out)
+        print(len(out))
+        break
+    
+    
 if __name__ == '__main__':
     main()
